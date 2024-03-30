09ad4f967d2163c449233466308cb5428b878226
mryunt02 <mrthresh58@gmail.com>
2024-03-30 04:43:52 +0300

    Using the CRUD Methods

--- src/command.js
+++ src/command.js
@@ -1,5 +1,13 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
+import { listNotes } from "./listnotes.js";
+import {
+  newNote,
+  getAllNotes,
+  removeNote,
+  findNotes,
+  removeAllNotes,
+} from "./notes.js";
 
 yargs(hideBin(process.argv))
   .command(
@@ -11,7 +19,11 @@ yargs(hideBin(process.argv))
         type: "string",
       });
     },
-    async (argv) => {}
+    async (argv) => {
+      const tags = argv.tags ? argv.tags.split(",") : [];
+      const note = await newNote(argv.note, tags);
+      console.log("Note created:", note);
+    }
   )
   .option("tags", {
     alias: "t",
@@ -22,7 +34,10 @@ yargs(hideBin(process.argv))
     "all",
     "get all notes",
     () => {},
-    async (argv) => {}
+    async (argv) => {
+      const notes = await getAllNotes();
+      listNotes(notes);
+    }
   )
   .command(
     "find <filter>",
@@ -34,7 +49,10 @@ yargs(hideBin(process.argv))
         type: "string",
       });
     },
-    async (argv) => {}
+    async (argv) => {
+      const matches = await findNotes(argv.filter);
+      listNotes(matches);
+    }
   )
   .command(
     "remove <id>",
@@ -45,7 +63,14 @@ yargs(hideBin(process.argv))
         description: "The id of the note you want to remove",
       });
     },
-    async (argv) => {}
+    async (argv) => {
+      const id = await removeNote(argv.id);
+      if (id) {
+        console.log("Note removed:", id);
+      } else {
+        console.log("Note not found");
+      }
+    }
   )
   .command(
     "web [port]",
@@ -63,7 +88,10 @@ yargs(hideBin(process.argv))
     "clean",
     "remove all notes",
     () => {},
-    async (argv) => {}
+    async (argv) => {
+      await removeAllNotes();
+      console.log("All notes removed");
+    }
   )
   .demandCommand(1)
   .parse();
